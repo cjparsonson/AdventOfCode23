82810547c856daf27909f23170b6aa17b0dcf65a
cjparsonson <cjparsonson@gmail.com>
2023-12-02 12:25:53 +0000

    Add tuples

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,14 +1,21 @@
 ﻿// Advent of Code 2023 Day 2 Puzzle
 
+// Definite cubeTuple list
+List<(string, int)> cubeTuples = new List<(string, int)>();
 
 string example =
     "Game 1: 1 blue, 2 green, 3 red; 7 red, 8 green; 1 green, 2 red, 1 blue; 2 green, 3 red, 1 blue; 8 green, 1 blue";
 
 // Split into game and cube components
 string[] gamesAndColours = example.Split(":");
+(string game, int gameNumber) gameTuple = (gamesAndColours[0], int.Parse(gamesAndColours[0].Split(" ")[1]));
+cubeTuples.Add(gameTuple);
+
 
 // Split into games
 string[] games = gamesAndColours[1].Split(";");
+
+
 
 // Split into colour cubes
 foreach (string game in games)
@@ -21,7 +28,15 @@ foreach (string game in games)
         // Split into colour and number
         string[] colourAndNumber = trimmedItem.Split(" ");
         WriteLine($"Colour: {colourAndNumber[1]} Number: {colourAndNumber[0]}");
+        (string colour, int number) cubeTuple = (colourAndNumber[1], int.Parse(colourAndNumber[0]));
+        cubeTuples.Add(cubeTuple);
     }
+}
+
+// Print out cubeTuples
+foreach (var cubeTuple in cubeTuples)
+{
+    WriteLine($"Text: {cubeTuple.Item1} Number: {cubeTuple.Item2}");
 }
 
 
